f177959caace7d68a1fe5a533d9174ec08264f56
Patrick Bourke <pbourke@gmail.com>
2017-09-20 10:46:41 -0700

    Added Common project for events and value types

---
+++ Common/Events/JobProposed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VaughnVernon.Mockroservices;
+
+namespace Common.Events
+{
+    public class JobProposed : IDomainEvent
+    {
+        public JobProposed()
+        {
+            EventVersion = 0;
+            OccurredOn = DateTime.UtcNow;
+        }
+
+        public int EventVersion { get; }
+        public DateTime OccurredOn { get; }
+    }
+}
