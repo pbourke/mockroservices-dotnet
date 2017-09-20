34f14d69b5e8b04521abc394ebaedca80a258259
Patrick Bourke <pbourke@gmail.com>
2017-09-20 10:23:47 -0700

    Added JobMatching and Pricing contexts

---
+++ ApplicationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApplicationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplicationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0028f836-9242-49e6-87e0-82e6391ba50d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JobMatchingContext/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaughnVernon.Mockroservices;
+
+namespace JobMatchingContext
+{
+    public class Job : EventSourcedRootEntity
+    {
+        protected Job(List<IDomainEvent> stream, int streamVersion) : base(stream, streamVersion)
+        {
+        }
+    }
+}
